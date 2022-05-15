1fb99428874b0e2d73af73853b5e1bcdaffba14b
Valerii Perepelytsia <perepelitsavn@ukr.net>
2022-05-15 11:34:33 +0300

    changes from lesson 28

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,21 @@
-import {getMovies} from "./api";
+import {getMovieDetails, getMovies} from "./api";
 
-getMovies('popular', '.popular');
-getMovies('top_rated', '.top-rated');
-getMovies('upcoming', '.upcoming');
+window.addEventListener('hashchange', e => {
+    const {location: {hash}} = window;
+    const [, movieId] = hash.split('=');
+
+    getMovieDetails(movieId, '.root');
+})
+
+const checkUrl = () => {
+    const {location: {hash}} = window;
+    const [hashName, movieId] = hash.split('=');
+
+    if(hashName === '#movieId') {
+        getMovieDetails(movieId, '.root');
+    } else {
+        getMovies('popular', '.root');
+    }
+}
+
+checkUrl();
